8e5f339d0e6181c2a8b915346875ccfa0c3fe6ef
Ismoil Nosirov <ismoilnasyr@gmail.com>
2026-02-12 11:50:37 -0500

    Adding ProgramControlTest

---
+++ src/test/java/ProgramControlTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramControlTest {
+
+    @Test
+    void testZeroInput() {
+    }
+
+    @Test
+    void testInvalidOneInput() {
+    }
+
+    @Test
+    void testInvalidTwoInput() {
+    }
+}
